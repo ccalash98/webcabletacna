e7befc1ed9f8fca4b0dfaf0e64f053a80d24e48a
ccalash98 <cristtofher.ccalash@gmail.com>
2024-10-10 22:30:47 -0500

    ajustes json y html

--- js/whatsapp.var.js
+++ js/whatsapp.var.js
@@ -4,27 +4,29 @@
 // Variable para almacenar la última URL utilizada
 let lastUrl = "";
 
-document.getElementById('randomLink').addEventListener('click', function(e) {
-    e.preventDefault(); // Evita la acción predeterminada del enlace
+// Seleccionar todos los enlaces con la clase "randomLink"
+document.querySelectorAll('.randomLink').forEach(function(element) {
+    element.addEventListener('click', function(e) {
+        e.preventDefault(); // Evita la acción predeterminada del enlace
 
-    // Lista de URLs
-    var urls = [
-        "https://api.whatsapp.com/send?phone=51900006000&text=Deseo%20más%20información%20de%20los%20servicios%20de%20Cable%20Tacna",
-        "https://api.whatsapp.com/send?phone=51900007000&text=Deseo%20más%20información%20de%20los%20servicios%20de%20Cable%20Tacna",
-        "https://api.whatsapp.com/send?phone=51900008000&text=Deseo%20más%20información%20de%20los%20servicios%20de%20Cable%20Tacna"
-    ];
+        // Lista de URLs
+        var urls = [
+            "https://api.whatsapp.com/send?phone=51900006000&text=Deseo%20más%20información%20de%20los%20servicios%20de%20Cable%20Tacna",
+            "https://api.whatsapp.com/send?phone=51900007000&text=Deseo%20más%20información%20de%20los%20servicios%20de%20Cable%20Tacna",
+            "https://api.whatsapp.com/send?phone=51900008000&text=Deseo%20más%20información%20de%20los%20servicios%20de%20Cable%20Tacna"
+        ];
 
-    var randomUrl = "";
+        var randomUrl = "";
 
-    // Seleccionar una URL aleatoria que no sea igual a la última utilizada
-    do {
-        randomUrl = urls[Math.floor(Math.random() * urls.length)];
-    } while (randomUrl === lastUrl);
+        // Seleccionar una URL aleatoria que no sea igual a la última utilizada
+        do {
+            randomUrl = urls[Math.floor(Math.random() * urls.length)];
+        } while (randomUrl === lastUrl);
 
-    // Actualizar la última URL seleccionada
-    lastUrl = randomUrl;
+        // Actualizar la última URL seleccionada
+        lastUrl = randomUrl;
 
-    // Abrir la URL en una nueva pestaña
-    window.open(randomUrl, '_blank');
+        // Abrir la URL en una nueva pestaña
+        window.open(randomUrl, '_blank');
+    });
 });
-
